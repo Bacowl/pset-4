cd720057384db3309f520c5d2ed05bd1489c62c7
Bacowl <44117000+Bacowl@users.noreply.github.com>
2019-11-18 16:15:12 -0500

    Update fibonacci.js

---
+++ src/fibonacci.js
@@ -0,0 +1,21 @@
+ 
+const readlineSync = require("readline-sync");
+
+console.log()
+
+var number = null;
+var fibonacci1 = 0;
+var fibonacci2 = 0;
+var fibonacciCurrent = 0;
+
+while (number < 1 || number % 1 != 0 || number > 78) {
+  var number= Number (readlineSync.question("Positive integer: "))
+}
+
+for (loop = 1; loop < number; loop++) {
+ var fibonacciCurrent = fibonacci2 + fibonacci1;
+ var fibonacci1 = fibonacci2;
+ var fibonacci2 = fibonacciCurrent;
+}
+
+Console.log(fibonacci2.toLocaleString("en") + ".")
